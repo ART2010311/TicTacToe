e3226f2ebfd03afa530efdfde01b3c4efe7e2708
ART2010311 <induthakur78687@gmail.com>
2024-04-01 21:33:53 +0545

    Initial commit

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final Button[][] buttons = new Button[3][3];
+    private boolean playerXTurn = true;
+    private int roundCount = 0;
+    private TextView textViewStatus;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewStatus = findViewById(R.id.message);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button" + i + j;
+                @SuppressLint("DiscouragedApi") int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+
+        }
+        Button buttonReset = findViewById(R.id.reset);
+        buttonReset.setOnClickListener(v -> resetGame());
+    }
+public void onClick(View v){
+        if(!((Button)v).getText().toString().equals("")){
+            return;
+    }
+    if(playerXTurn){
+        ((Button)v).setText("X");
+
+    }else{
+        ((Button)v).setText("0");
+
+    }
+    roundCount++;
+    if(checkForWinner()) {
+        if (playerXTurn) {
+            updateStatus("Player X wins!");
+        } else {
+            updateStatus("Player 0 wins!");
+        }
+    } else if (roundCount==9) {
+        updateStatus("Its a draw");
+    }else{
+        playerXTurn=!playerXTurn;
+        updateStatus("Next turn:"+(playerXTurn ? "x":"0"));
+
+    }
+
+}
+private boolean checkForWinner() {
+    String[][] field = new String[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            field[i][j] = buttons[i][j].getText().toString();
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")) {
+            return true;
+        }
+        if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
+            return true;
+        }
+    }
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")) {
+            return true;
+        }
+        return (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals(""));
+
+    }
+
+
+        private void updateStatus(String message){ textViewStatus.setText(message);}
+
+
+
+
+   private void resetGame(){
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    buttons[i][j].setText("");
+                }
+            }
+            playerXTurn=true;
+            roundCount=0;
+            updateStatus("Next turn: X");
+    }
+}
